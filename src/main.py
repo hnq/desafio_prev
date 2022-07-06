a144e180f1c0819dd5dfc13ed4ef4284966a1311
hnq <hnq_campos@hotmail.com>
2022-07-06 10:09:53 -0300

    commit initial

---
+++ src/main.py
@@ -0,0 +1,31 @@
+from banco_imobiliario.config import settings
+
+from banco_imobiliario.board.factory import create_board
+from banco_imobiliario.board.game_statistics import printer_results
+
+
+def main():
+    '''
+    Uma execução do programa proposto deve rodar 300
+    simulações, imprimindo no console os dados
+    referentes às execuções.
+    '''
+    results = []
+    for index in range(int(settings.ENV_NUMBER_SIMULATION)):
+        board = create_board()
+        while board.winner is None:
+            for player in board.players:
+                if player.gameover:
+                    board.remove(player)
+                winner = board.check_winner(player)
+                if winner:
+                    board.winner = winner
+                    break
+                board.play(player, board)
+            board.played += 1
+        results.append(board.finish())
+    printer_results(results)
+
+
+if __name__ == "__main__":
+    main()
